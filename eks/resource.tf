4cc77e500bc3adcf72548b18c020cf70be2c5eed
vidya-udayakumar <111853170+vidya-udayakumar@users.noreply.github.com>
2023-01-20 18:03:31 +0530

    Update resource.tf

--- eks/resource.tf
+++ eks/resource.tf
@@ -1,5 +1,5 @@
 module "eks-dev" {
-    source = "./module"
+    source = "../modules/eks"
     name = "clusscope"
     subnet_ids = ["subnet-0a072ab649998332a", "subnet-0e31d001d74f5fd4a"]
     cluster_version = "1.24"
