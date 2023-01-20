711fc59f6f9adcc1b6fadee64b2f72c2c0111277
vidya-udayakumar <111853170+vidya-udayakumar@users.noreply.github.com>
2023-01-20 15:59:37 +0530

    Update backend.tf

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
-    bucket               = "bucket-forterraform"
-    region               = "ap-southeast-1"
-    key                  = "bucket-forterraform/resources/vpc/terraform.tfstate"
+    bucket = "bucket-forterraform"
+    key    = "bucket-forterraform/resources/vpc/terraform.tfstate/"
   }
 }
