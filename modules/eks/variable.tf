f8c8191d7ed9e4af1ff6de61fd2c8e790a19aa43
vidya-udayakumar <111853170+vidya-udayakumar@users.noreply.github.com>
2023-01-23 14:07:17 +0530

    Update variable.tf

--- modules/eks/variable.tf
+++ modules/eks/variable.tf
@@ -1,9 +1,47 @@
-variable "name" {
+variable "common_tags" {
+  type = map(any)
+  default = {
+    env     = "dev"
+    project = "control_plane"
+    region  = "ap-southeast-1"
+  }
 }
 
-variable "subnet_ids" {
-    type = list
+
+variable "cluster_name" {
+  type = string
 }
 
-variable "cluster_version" {
+variable "vpc_id" {
+  type = string
+}
+
+variable "cluster_subnet_ids" {
+  type = list(string)
+}
+
+variable "fargate_profiles" {
+  type    = any
+  default = {}
+  ######sample input#################
+  # backend = {
+  #   name = join("-", [local.prefix, "core"])
+  #   selectors = [
+  #     {
+  #       namespace = local.prefix
+  #       labels = {
+  #         "app.kubernetes.io/name" = "core"
+  #       }
+  #     }
+  #   ]
+  #   tags = merge(local.common_tags, tomap({ Namespace = "Default" }))
+  #   timeouts = {
+  #     create = "20m"
+  #     delete = "20m"
+  #   }
+  # },
+}
+
+variable "fargate_profile_subnet_ids" {
+  type = list(string)
 }
