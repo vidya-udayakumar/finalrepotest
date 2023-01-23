a26745cb74f15c9ef8856e8ca9724741a6d80aef
vidya-udayakumar <111853170+vidya-udayakumar@users.noreply.github.com>
2023-01-23 14:09:08 +0530

    Update resource.tf

--- eks/resource.tf
+++ eks/resource.tf
@@ -1,8 +1,26 @@
-module "eks-dev" {
-    source = "../modules/eks"
-    name = "clusscope"
-    subnet_ids = ["subnet-0a072ab649998332a", "subnet-0e31d001d74f5fd4a"]
-    cluster_version = "1.24"
-}
+    module "eks" {
+  source = "../modules/eks"
 
+  cluster_name       = "ts-cp-dev-eks-1"
+  vpc_id             = "vpc-0e3953cb26597b2f0"
+  cluster_subnet_ids = ["subnet-02cd374dcc97ccbaf","subnet-0a5377b67f1a03afb"]
+
+  fargate_profiles = {
+    fp-default = {
+      name      = "fp-default"
+      selectors = [{ namespace = "default" }, { namespace = "kube-system" }]
+    },
+    alb-sample-app = {
+      name      = "alb-sample-app"
+      selectors = [{ namespace = "fp-dev" }]
+    }
+  }
+  fargate_profile_subnet_ids = ["subnet-02cd374dcc97ccbaf","subnet-0a5377b67f1a03afb"]
+  
+  common_tags = {
+    env     = "dev"
+    project = "terrascope"
+    region  = "ap-southeast-1"
+  }
+}
     
